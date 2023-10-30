3d4ecea2c0aa7821dab38e43c4037100a63d58fa
Nukutach <114015993+Nukutach@users.noreply.github.com>
2023-10-30 12:50:06 +0300

    Add files via upload

---
+++ task4/Task4.java
@@ -0,0 +1,34 @@
+package task4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(args[0]))) {
+            while (scanner.hasNext()) {
+                array.add(scanner.nextInt());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int size = array.size();
+        if (size < 2)
+            throw new IllegalArgumentException("array's size < 2");
+
+        Collections.sort(array);
+        int avg = array.get(size / 2);
+        int sum = 0;
+        for (int a : array) {
+            sum += Math.abs(a - avg);
+        }
+        System.out.println(sum);
+    }
+}
